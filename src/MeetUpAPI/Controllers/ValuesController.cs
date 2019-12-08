f0a4f277d41efa21a35d7c99a906372fa2a7db92
admin.az <andrej.zaujec@propivot.com>
2019-12-09 00:42:25 +0100

    Adding WebApi component to solution

---
+++ src/MeetUpAPI/Controllers/ValuesController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetUpAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        string connectionStr = @"Data Source=localhost;Initial Catalog=MeetUpDB;User ID=sa;Password=Test@12345";
+        
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<dynamic>>> Get()
+        {          
+
+            using (var conn = new SqlConnection(connectionStr)) 
+            {
+                IEnumerable<dynamic> hosts = await conn.QueryAsync<dynamic>(
+                    sql: @"SELECT * FROM [dbo].[hosts]"
+                    );
+
+                return Ok(hosts);
+            }
+
+             //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<dynamic>>> Get(int id)
+        {
+            return Ok(await getSpeakersByMeetUpId(id));
+        }
+
+        public async Task<IEnumerable<dynamic>> getSpeakersByMeetUpId(int id)
+        {
+            using (var conn = new SqlConnection(connectionStr))
+            {
+                IEnumerable<dynamic> speakers = await conn.QueryAsync<dynamic>(
+                    sql: @"SELECT * FROM [dbo].[speakers] WHERE [meetupId] = @id",
+                    param: new { id }
+                    );
+
+                return speakers;
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+    }
+}
---
+++ src/MeetUpAPITests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MeetUpAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace MeetUpAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class ValuesControllerTests
+    {
+        string connectionStr = @"Data Source=localhost;Initial Catalog=MeetUpDB;User ID=sa;Password=Test@12345";
+
+        [TestMethod()]
+        public void GetSpeakersTest()
+        {
+            using (var conn = new SqlConnection(connectionStr))
+            {
+                IEnumerable<dynamic> speakers = conn.Query<dynamic>(
+                    sql: @"SELECT * FROM [dbo].[speakers]"
+                    );
+
+                Assert.IsTrue(speakers.AsList().Count == 2);
+            }           
+        }
+
+        [TestMethod()]
+        public void GetHostsTest()
+        {
+            using (var conn = new SqlConnection(connectionStr))
+            {
+                IEnumerable<dynamic> speakers = conn.Query<dynamic>(
+                    sql: @"SELECT * FROM [dbo].[hosts]"
+                    );
+
+                Assert.IsTrue(speakers.AsList().Count == 1);
+            }
+        }
+
+        [TestMethod()]
+        public void AddHostsTest()
+        {
+            using (var conn = new SqlConnection(connectionStr))
+            {
+                string name = "superman";
+
+                IEnumerable<dynamic> speakers = conn.Query<dynamic>(
+                    sql: @"INSERT INTO [dbo].[hosts] (fullName) OUTPUT inserted.* VALUES (@name)",
+                    param: new { name }
+                    );
+
+                Assert.IsTrue(speakers.AsList().Count == 1);
+            }
+        }
+    }
+}
